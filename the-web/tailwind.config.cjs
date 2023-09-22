24a55944fd982fb96309055fe2c6265509ef1af0
Joshua Santos <JoshCSan@gmail.com>
2023-09-21 23:16:44 -0700

    - wip: idk

--- the-web/tailwind.config.cjs
+++ the-web/tailwind.config.cjs
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -9,5 +11,24 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [plugin(function ({ addUtilities }) {
+    addUtilities({
+      '.safe-top': {
+        paddingTop: 'constant(safe-area-inset-top)',
+        paddingTop: 'env(safe-area-inset-top)'
+      },
+      '.safe-bottom': {
+        paddingBottom: 'constant(safe-area-inset-bottom)',
+        paddingBottom: 'env(safe-area-inset-bottom)'
+      },
+      '.safe-left': {
+        paddingLeft: 'constant(safe-area-inset-left)',
+        paddingLeft: 'env(safe-area-inset-left)'
+      },
+      '.safe-right': {
+        paddingRight: 'constant(safe-area-inset-right)',
+        paddingRight: 'env(safe-area-inset-right)'
+      },
+    })
+  })],
 };
