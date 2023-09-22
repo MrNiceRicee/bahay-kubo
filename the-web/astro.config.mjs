2242f179c710b2863c11d054ced461d4a10bb588
Joshua Santos <JoshCSan@gmail.com>
2023-09-21 23:02:01 -0700

    - wip: it's a start

--- the-web/astro.config.mjs
+++ the-web/astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
-  })]
+  })],
+  server: {
+    host: '0.0.0.0',
+  }
 });
